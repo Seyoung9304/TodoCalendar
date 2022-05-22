60d8948ae6ce3c515f2ff90bec4a626836dd4886
Seyoung9304 <seftware99@gmail.com>
2022-05-22 21:40:33 +0900

    [fix] duplicate class error

--- app/src/main/java/edu/skku/cs/todocalendar/CalendarActivity.java
+++ app/src/main/java/edu/skku/cs/todocalendar/CalendarActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
+
 public class CalendarActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
