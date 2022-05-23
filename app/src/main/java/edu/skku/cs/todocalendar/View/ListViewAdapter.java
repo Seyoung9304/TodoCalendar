bce675903aa85c376af584e01792fef515e17cae
Seyoung9304 <seftware99@gmail.com>
2022-05-24 00:34:20 +0900

    [add] todo listview

---
+++ app/src/main/java/edu/skku/cs/todocalendar/View/ListViewAdapter.java
@@ -0,0 +1,59 @@
+package edu.skku.cs.todocalendar.View;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import edu.skku.cs.todocalendar.Classes.Plan;
+import edu.skku.cs.todocalendar.R;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private ArrayList<Plan> plans;
+
+    public ListViewAdapter(Context mContext, ArrayList<Plan> plans){
+        this.mContext = mContext;
+        this.plans = plans;
+    }
+
+    @Override
+    public int getCount() {
+        return plans.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return plans.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view==null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.listviewlayout, viewGroup, false);
+        }
+        Button todo = view.findViewById(R.id.todo_object);
+        String color =  plans.get(i).getColor();
+        // TODO
+        switch (color) {
+            case "purple":
+                // todo.setBackgroundColor();
+                break;
+        }
+
+        return view;
+    }
+}
