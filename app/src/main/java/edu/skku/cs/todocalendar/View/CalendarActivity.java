1f45c1b5547879afc517cc6f39070644a877015b
Seyoung9304 <seftware99@gmail.com>
2022-05-28 19:40:44 +0900

    [fix] Calendar: Calendarview -> RecyclerView [feat] CalendarViewHolder, Adapter

--- app/src/main/java/edu/skku/cs/todocalendar/View/CalendarActivity.java
+++ app/src/main/java/edu/skku/cs/todocalendar/View/CalendarActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,7 +15,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import edu.skku.cs.todocalendar.Classes.Plan;
@@ -23,8 +29,12 @@ import edu.skku.cs.todocalendar.Presenter.MainPresenter;
 import edu.skku.cs.todocalendar.R;
 
 
-public class CalendarActivity extends AppCompatActivity implements CalendarContract.View {
-    CalendarView calendarView;
+public class CalendarActivity extends AppCompatActivity implements CalendarContract.View, CalendarAdapter.OnItemListener {
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
+
     AppCompatButton addplan;
     ListView listview;
     ListViewAdapter listViewAdapter;
@@ -33,7 +43,7 @@ public class CalendarActivity extends AppCompatActivity implements CalendarContr
     ArrayList<Plan> todayplans;
 
     private String uid;
-    long selectedDate;
+    // long selectedDate;
 
     CalendarContract.Presenter presenter;
 
@@ -43,6 +53,10 @@ public class CalendarActivity extends AppCompatActivity implements CalendarContr
         setContentView(R.layout.activity_calendar);
         presenter = new CalendarPresenter(this);
 
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
+
         uid = getIntent().getStringExtra("uid");
 
         new Thread(){
@@ -51,27 +65,88 @@ public class CalendarActivity extends AppCompatActivity implements CalendarContr
             }
         }.start();
 
-        calendarView = findViewById(R.id.calendarview);
+        // calendarView = findViewById(R.id.calendarview);
         listview = findViewById(R.id.todo_list);
         addplan = findViewById(R.id.b_add_todo);
-
-        selectedDate = calendarView.getDate();
 
         addplan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), TodoActivity.class);
+                //intent.putExtra("*/-+")
                 startActivity(intent);
             }
         });
-
+        /*
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                presenter.onDateClick(year, month+1, day); //month: 0~11
+
             }
-        });
+        });*/
     }
+
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i = 1; i <= 42; i++) {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
+                daysInMonthArray.add("");
+            } else {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return  daysInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public void previousMonthAction(View view){
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view){
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if(!dayText.equals("")) {
+            presenter.onDateClick(selectedDate.getYear(), selectedDate.getMonthValue()+1, Integer.parseInt(dayText)); //month: 0~11
+            // String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            // Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     public void showTodayTodoList(ArrayList<Plan> today_plans) {
---
+++ app/src/main/java/edu/skku/cs/todocalendar/View/CalendarViewHolder.java
@@ -0,0 +1,27 @@
+package edu.skku.cs.todocalendar.View;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.skku.cs.todocalendar.R;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+    {
+        super(itemView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+    }
+}
